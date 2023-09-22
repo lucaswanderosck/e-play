891a1d7007d78b1749ce07e2481f55a93b168eff
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-09-22 14:53:24 -0300

    componentizando as seções

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from "./styles";
+
+export type Props = {
+  title: string;
+  background: "black" | "gray";
+  children: React.ReactNode;
+};
+
+export const Section = ({ title, background, children }: Props) => {
+  return (
+    <Container background={background}>
+      <div className="container">
+        <h2>{title}</h2>
+        {children}
+      </div>
+    </Container>
+  );
+};
---
+++ src/components/Section/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+import { Container as CardContainer } from "../Product/styles";
+
+import { Props } from ".";
+import { colors } from "../../styles/GlobalStyles";
+
+export const Container = styled.section<Omit<Props, "title" | "games">>`
+  padding: 32px 0;
+  background-color: ${(props) =>
+    props.background === "black" ? colors.black : colors.gray};
+
+  ${CardContainer} {
+    background-color: ${(props) =>
+      props.background === "black" ? colors.gray : colors.black};
+  }
+
+  h2 {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 24px;
+  }    
+
+  p{
+    font-size: 14px;
+    line-height: 22px;
+    max-width: 640px;
+  }
+`;
