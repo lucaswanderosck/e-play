2a13c39acc7573794d2ee2a31f4244839da61c1d
Lucas Wanderosck <lucaskleinwanderosck@gmail.com>
2024-03-20 13:17:50 -0300

    Adiciona opção de pagamento com boleto e cartão de crédito no Checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
+import { FaBarcode, FaCreditCard } from 'react-icons/fa'
 import { Button } from '../../components/Button'
 import { Card } from '../../components/Card'
-import { Container, InputGroup, Row } from './styles'
+import { Container, InputGroup, Row, TabButton } from './styles'
 
 export const Checkout = () => {
+  const [payWithCard, setPayWithCard] = useState(true)
+
   return (
     <Container className="container">
       <Card title="Dados de Cobrança">
@@ -35,14 +39,72 @@ export const Checkout = () => {
         </>
       </Card>
       <Card title="Pagamento">
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora a
-            ipsum nostrum molestias reprehenderit alias quis, vel, perferendis
-            quisquam excepturi animi. Similique facilis consequuntur optio magni
-            atque eius. Sapiente, optio!
-          </p>
-        </div>
+        <>
+          <TabButton
+            active={!payWithCard}
+            onClick={() => setPayWithCard(false)}
+          >
+            <FaBarcode />
+            <span>Boleto</span>
+          </TabButton>
+          <TabButton active={payWithCard} onClick={() => setPayWithCard(true)}>
+            <FaCreditCard />
+            <span>Cartão de Crédito</span>
+          </TabButton>
+          {payWithCard ? (
+            <>
+              <Row>
+                <InputGroup>
+                  <label htmlFor="nameCard">Nome do titular</label>
+                  <input type="text" id="nameCard" />
+                </InputGroup>
+                <InputGroup>
+                  <label htmlFor="cardCpf">CPF do titular</label>
+                  <input type="text" id="cardCpf" />
+                </InputGroup>
+              </Row>
+              <Row marginTop="24px">
+                <InputGroup>
+                  <label htmlFor="CardDisplayName">Nome no Cartão</label>
+                  <input type="text" id="CardDisplayName" />
+                </InputGroup>
+                <InputGroup>
+                  <label htmlFor="cardNumber">Numero do Cartão</label>
+                  <input type="text" id="cardNumber" />
+                </InputGroup>
+                <InputGroup maxWidth="123px">
+                  <label htmlFor="cardExpirationMouth">Mes de Expiração</label>
+                  <input type="text" id="cardExpiration" />
+                </InputGroup>
+                <InputGroup maxWidth="123px">
+                  <label htmlFor="cardExpirationYear">Ano de Expiração</label>
+                  <input type="text" id="cardExpiration" />
+                </InputGroup>
+                <InputGroup maxWidth="48px">
+                  <label htmlFor="cardCvv">CVV</label>
+                  <input type="text" id="cardCvv" />
+                </InputGroup>
+              </Row>
+              <Row marginTop="24px">
+                <InputGroup maxWidth="140px">
+                  <label htmlFor="parcela">Parcelamento</label>
+                  <select name="parcela" id="parcela">
+                    <option value="1">1x R$ 100,00</option>
+                    <option value="2">2x R$ 50,00</option>
+                    <option value="3">3x R$ 33,33</option>
+                  </select>
+                </InputGroup>
+              </Row>
+            </>
+          ) : (
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora a
+              ipsum nostrum molestias reprehenderit alias quis, vel, perferendis
+              quisquam excepturi animi. Similique facilis consequuntur optio
+              magni atque eius. Sapiente, optio!
+            </p>
+          )}
+        </>
       </Card>
       <Button type="button" title="Finalizar a compra">
         Finalizar Compra
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -3,12 +3,24 @@ import { colors } from '../../styles/GlobalStyles'
 
 export const Container = styled.div``
 
-export const Row = styled.div`
+type RowProps = {
+  marginTop?: string
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
   column-gap: 24px;
+  align-items: flex-end;
+  margin-top: ${({ marginTop }) => marginTop || '0'};
 `
-export const InputGroup = styled.div`
+
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+  max-width: ${({ maxWidth }) => maxWidth || 'auto'};
 
   label {
     font-size: 14px;
@@ -17,11 +29,34 @@ export const InputGroup = styled.div`
     margin-bottom: 8px;
   }
 
-  input {
+  input,
+  select {
     border: 1px solid ${colors.white};
     background-color: ${colors.white};
     height: 32px;
     padding: 0 8px;
     width: 100%;
+  }
+`
+
+type TabButtonProps = {
+  active?: boolean
+}
+
+export const TabButton = styled.button<TabButtonProps>`
+  margin-bottom: 24px;
+  font-size: 14px;
+  font-weight: bold;
+  background-color: ${({ active }) => (active ? colors.green : colors.black)};
+  border: none;
+  color: ${colors.white};
+  cursor: pointer;
+  height: 32px;
+  border-radius: 8px;
+  margin-right: 16px;
+  padding: 0 8px;
+
+  svg {
+    margin-right: 8px;
   }
 `
